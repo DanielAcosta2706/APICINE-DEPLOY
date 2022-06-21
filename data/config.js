08743d0c75fbcb35cdff84869efa92cd4cc9f5a3
DanielAcosta2706 <danyel2706@gmail.com>
2022-06-21 09:59:24 -0300

    database connections updated

--- data/config.js
+++ data/config.js
@@ -9,6 +9,7 @@ const pool = mysql.createPool({
   host: process.env.db_host,
   database: process.env.db_name,
   user: process.env.db_user,
+  password: process.env.db_pass,
 });
 
 // Coneccion a la base de datos
